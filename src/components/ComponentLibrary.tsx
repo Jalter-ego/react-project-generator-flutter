f15bdf5d1e4afad5897aefeaa3a535232143713c
Jalter-ego <jhademels123@gmail.com>
2025-06-03 23:01:58 -0400

    feat: update components for only change

--- src/components/ComponentLibrary.tsx
+++ src/components/ComponentLibrary.tsx
@@ -1,51 +1,52 @@
-// components/ComponentLibrary.tsx
+import { IconLock, IconMenuDeep, IconSearch, IconUser } from '../assets/Icons';
 
-import { IconLock, IconMenuDeep, IconSearch, IconUser } from "../assets/Icons";
-
-export function Button() {
+export function Button({ label = 'Botón', bg = '#45def2', width = 128, height = 32 }:
+   { label?: string; bg?: string; width?: number; height?: number }) {
   return (
-    <button className="bg-cyan-500 text-white w-32 h-8 rounded-xl font-semibold
-        cursor-pointer">
-      Botón
+    <button
+      className={` text-white rounded-xl font-semibold cursor-pointer`}
+      style={{ width: `${width}px`, height: `${height}px`, backgroundColor: bg }}
+    >
+      {label}
     </button>
   );
 }
 
-export function TextField() {
+export function TextField({ label = "", placeholder = 'Campo de texto', width = 232, height = 32 }:
+{ label?: string; placeholder?: string; width?: number; height?: number }) {
   return (
     <input
-      className="w-58 h-8 rounded-xl border border-zinc-300 px-2 text-black"
-      placeholder="Campo de texto"
+      className="rounded-xl border border-zinc-300 px-2 text-black"
+      placeholder={placeholder}
+      value={label}
+      style={{ width: `${width}px`, height: `${height}px` }}
     />
   );
 }
 
-export function Checkbox(){
-  return(
-    <input type="checkbox" 
-      className="w-4 h-4 bg-white"
-    />
-  )
+export function Checkbox({ checked = false }: { checked?: boolean }) {
+  return <input type="checkbox" className="w-4 h-4 bg-white" defaultChecked={checked} />;
 }
 
-export function AppBar1(){
-  return(
-    <nav className="w-[300px] h-8 flex items-center justify-between text-zinc-700
-      px-4">
-      <IconMenuDeep/>
-      <IconUser/>
+export function AppBar1({ width = 300, height = 32 }: { width?: number; height?: number }) {
+  return (
+    <nav
+      className="flex items-center justify-between text-zinc-700 px-4"
+      style={{ width: `${width}px`, height: `${height}px` }}
+    >
+      <IconMenuDeep />
+      <IconUser />
     </nav>
-  )
+  );
 }
 
-// Diccionario de componentes para renderizado dinámico
-export const componentMap: Record<string, () => React.JSX.Element> = {
+export const componentMap: Record<string, (props: any) => React.JSX.Element> = {
   button: Button,
   textfield: TextField,
   checkbox: Checkbox,
-  appbar1:AppBar1,
+  appbar1: AppBar1,
   iconUser: IconUser,
   iconSearch: IconSearch,
   iconLock: IconLock,
-  iconMenuDeep: IconMenuDeep
+  iconMenuDeep: IconMenuDeep,
 };
