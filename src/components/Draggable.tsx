549bc0ff97dbdca4bb8ed2da8d27c6905219936a
Jalter-ego <jhademels123@gmail.com>
2025-06-05 22:48:20 -0400

    feat: update sidebar components

--- src/components/Draggable.tsx
+++ src/components/Draggable.tsx
@@ -2,7 +2,8 @@
 import { useDraggable } from '@dnd-kit/core';
 //import { CSS } from '@dnd-kit/utilities';
 
-export function Draggable({ id, label, children }: { id: string; label: string; children: React.ReactNode }) {
+export function Draggable({ id, label, children, section }:
+  { id: string; label: string; children: React.ReactNode; section: string }) {
   const { attributes, listeners, setNodeRef } = useDraggable({ id });
 
   const style = {
@@ -11,20 +12,35 @@ export function Draggable({ id, label, children }: { id: string; label: string; 
   };
 
   return (
-    <div
-      ref={setNodeRef}
-      {...attributes}
-      {...listeners}
-      style={style}
-      className="rounded-lg bg-zinc-900 hover:bg-zinc-800 transition p-3 shadow border border-zinc-700 cursor-pointer"
-    >
-      <div className="bg-white text-black rounded-md p-2 flex justify-center items-center mb-1">
-        {children}
-      </div>
-      <p className="text-xs text-center text-zinc-300 group-hover:text-white
-        ">
-        {label}
-      </p>
-    </div>
+    <>
+      {section === "icons" ? (
+        <div
+          ref={setNodeRef}
+          {...attributes}
+          {...listeners}
+          style={style}
+          className="rounded-lg bg-zinc-900 hover:bg-zinc-800 transition p-3 shadow border border-zinc-700 cursor-pointer"
+        >
+          <div className="bg-white text-black rounded-md p-2 flex justify-center items-center mb-1">
+            {children}
+          </div>
+          <p className="text-xs text-zinc-300">{label}</p>
+
+        </div>
+      ) : (
+        <div
+          ref={setNodeRef}
+          {...attributes}
+          {...listeners}
+          style={style}
+          className="rounded-lg border border-zinc-700 transition p-1 shadow cursor-pointer w-full"
+        >
+            {children}
+
+        </div>
+      )
+
+      }
+    </>
   );
 }
--- src/components/SidebarComponents.tsx
+++ src/components/SidebarComponents.tsx
@@ -1,6 +1,45 @@
-import { IconLock, IconMenuDeep, IconSearch, IconUser } from "../assets/Icons";
+import { IconLock, IconMenuDeep, IconSearch, IconTable, IconUser } from "../assets/Icons";
 import { AppBar1, Button, Checkbox, TextField } from "./ComponentLibrary";
 import { Draggable } from "./Draggable";
+import { Table } from "./library/Table";
+
+export const components = [
+    {
+        id: "button",
+        label: "Botón",
+        icon: IconTable,
+        component: Button,
+        bg: "#18181b",
+    },
+    {
+        id: "textfield",
+        label: "TextField",
+        icon: IconTable,
+        component: TextField,
+        bg: "#18181b",
+    },
+    {
+        id: "checkbox",
+        label: "Checkbox",
+        icon: IconTable,
+        component: Checkbox,
+        bg: "#18181b",
+    },
+    {
+        id: "appbar1",
+        label: "AppBar",
+        icon: IconTable,
+        component: AppBar1,
+        bg: "#18181b"
+    },
+    {
+        id: "table",
+        label: "Table",
+        icon: IconTable,
+        component: Table,
+        bg: "#18181b"
+    }
+]
 
 export default function SidebarComponents() {
     const icons = [
@@ -21,18 +60,35 @@ export default function SidebarComponents() {
             icon: IconMenuDeep
         }
     ];
+
+
+
     return (
-        <aside className="w-[250px] h-screen bg-[#1f1f1f] flex flex-col py-6 gap-4 border-r border-zinc-700 shadow-lg">
+        <aside className="w-[250px] h-screen overflow-y-auto bg-[#1f1f1f] flex flex-col py-6 gap-4 border-r border-zinc-700 shadow-lg">
 
             <header className='border-b border-zinc-600 w-full flex px-2 pb-3'>
                 <h1 className='font-semibold text-[14px]'>Componentes</h1>
             </header>
 
-            <nav className="flex flex-col gap-2 px-2">
-                <Draggable id="button" label="Botón"><Button /></Draggable>
-                <Draggable id="textfield" label="Campo de texto"><TextField /></Draggable>
-                <Draggable id="checkbox" label="Checkbox"><Checkbox /></Draggable>
-                <Draggable id="appbar1" label="AppBar"><AppBar1 /></Draggable>
+            <nav className="grid grid-cols-2 gap-2 px-2">
+                {components.map((component) => (
+                    <Draggable
+                        key={component.id}
+                        id={component.id}
+                        label={component.label}
+                        section="components"
+                    >
+                        <div
+                            className="flex w-full items-center gap-1 p-2 rounded-lg shadow-md
+                             text-white hover:scale-[1.02] transition-transform "
+                            style={{ backgroundColor: component.bg }}
+                        >
+                            <component.icon/>
+                            <span className="text-[12px] font-medium">{component.label}</span>
+                        </div>
+                    </Draggable>
+                ))}
+
             </nav>
 
             {/* Sección de íconos en grid */}
@@ -40,7 +96,7 @@ export default function SidebarComponents() {
                 <h2 className="text-xs font-semibold text-zinc-400 mb-2">Iconos</h2>
                 <div className="grid grid-cols-3 gap-2">
                     {icons.map((Icon) => (
-                        <Draggable key={Icon.name} id={Icon.name} label={Icon.name}>
+                        <Draggable key={Icon.name} id={Icon.name} label={Icon.name} section="icons">
                             <Icon.icon />
                         </Draggable>
                     ))}
