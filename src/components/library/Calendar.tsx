ebb5864d09f3b2466126131280d9ad09935e32d5
Jalter-ego <jhademels123@gmail.com>
2025-06-09 08:05:26 -0400

    fixed: delete package for conflict version

--- src/components/library/Calendar.tsx
+++ src/components/library/Calendar.tsx
@@ -1,14 +1,25 @@
-import { Calendar } from "@/components/ui/calendar"
-import { useState } from "react"
+import { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css"; 
 
 export default function CalendarComponent() {
-    const [date, setDate] = useState<Date | undefined>(new Date())
-    return (
-        <Calendar
-            mode="single"
-            selected={date}
-            onSelect={setDate}
-            className="rounded-md border"
-        />
-    )
+  const [date, setDate] = useState<Date | null>(new Date());
+
+  return (
+    <div
+      style={{
+        width: "290px",
+        padding: "8px",
+      }}
+    >
+      <Calendar
+        value={date}
+        onChange={(newDate) => setDate(newDate as Date)}
+        className="custom-calendar"
+        calendarType="gregory"
+        showNavigation={true}
+        
+      />
+    </div>
+  );
 }
