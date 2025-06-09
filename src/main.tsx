977e41d1c75668efbc1be6d6a661107630a8e94b
Jalter-ego <jhademels123@gmail.com>
2025-06-08 23:34:05 -0400

    feat: added and use user contex

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ClerkProvider } from '@clerk/clerk-react'
-import './index.css'
-import { Router } from './router'
 import { BrowserRouter } from 'react-router-dom'
+import { Router } from './router'
+import { Toaster } from 'sonner'
+import './index.css'
+import { UserProvider } from './hooks/userContext'
+import SyncUserWithBackend from './hooks/useSignIn'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -16,7 +19,11 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
       <BrowserRouter>
-        <Router />
+        <UserProvider>
+          <SyncUserWithBackend/>
+          <Router />
+          <Toaster />
+        </UserProvider>
       </BrowserRouter>
     </ClerkProvider>
   </StrictMode>,
